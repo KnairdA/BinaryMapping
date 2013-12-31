962fa2f956349590bd2df040d451abbef5b98ff2
Adrian Kummerländer <KnairdA@t-online.de>
2013-12-31 14:16:55 +0100

    Extended Buffer and added BufferIterator template * Buffer template will be used as a container instead of a primitive scope guard * Access to memory will be abstracted through BufferIterator or Buffer operators * Buffer template is not specialized on a size as one may want to map diffently sized tuples on a single buffer * Buffer template size specialization is achieved through template methods * Library doesn't build at the moment as the buffer changes are not carried through into all templates using the Buffer

--- src/io/buffer.h
+++ src/io/buffer.h
@@ -1,30 +1,64 @@
 #ifndef BINARY_MAPPING_SRC_IO_BUFFER_H_
 #define BINARY_MAPPING_SRC_IO_BUFFER_H_
 
+#include <stdexcept>
+
+#include "buffer_iterator.h"
+
 namespace BinaryMapping {
 
 class Buffer {
 	public:
 		Buffer(uint8_t* d, size_t s):
-			data(d),
-			size(s),
+			data_(d),
+			size_(s),
 			owner_(true) { }
 
-		Buffer(const Buffer& source):
-			data(source.data),
-			size(source.size),
+		Buffer(const Buffer& src):
+			data_(src.data_),
+			size_(src.size_),
 			owner_(false) { }
 
 		~Buffer() {
 			if ( this->owner_ ) {
-				std::free(this->data);
+				std::free(this->data_);
 			}
 		}
 
-		uint8_t* data;
-		const size_t size;
+		template <size_t Size>
+		inline uint8_t* at(size_t index) const {
+			if ( index < this->size<Size> ) {
+				return this->data_ + index * Size;
+			} else {
+				throw std::out_of_range("range_violated");
+			}
+		}
+
+		inline uint8_t* operator[] (size_t index) const {
+			return this->data_ + index;
+		}
+
+		template <size_t Size>
+		inline BufferIterator<Size> begin() const {
+			return BufferIterator<Size>(this->data_);
+		}
+
+		template <size_t Size>
+		inline BufferIterator<Size> end() const {
+			return BufferIterator<Size>(
+				this->data_ + this->size<Size>() * Size
+			);
+		}
+
+		template <size_t Size>
+		inline size_t size() const {
+			return this->size_ / Size;
+		}
 
 	private:
+		uint8_t* const data_;
+		const size_t size_;
+
 		const bool owner_;
 };
 
---
+++ src/io/buffer_iterator.h
@@ -0,0 +1,80 @@
+#ifndef BINARY_MAPPING_SRC_IO_BUFFER_ITERATOR_H_
+#define BINARY_MAPPING_SRC_IO_BUFFER_ITERATOR_H_
+
+#include <iterator>
+
+namespace BinaryMapping {
+
+template <size_t Size>
+class BufferIterator : public std::iterator<std::random_access_iterator_tag,
+                              uint8_t*,
+                              off_t,
+                              uint8_t**,
+                              uint8_t*&> {
+	public:
+		BufferIterator(uint8_t* buffer):
+			index_ptr_(buffer) { }
+
+		inline bool operator==(const BufferIterator<Size>& src) const {
+			return this->index_ptr_ == src.index_ptr_;
+		}
+
+		inline bool operator!=(const BufferIterator<Size>& src) const {
+			return !(this == src);
+		}
+
+		inline uint8_t* operator*() {
+			return this->index_ptr_;
+		}
+
+		inline BufferIterator<Size>& operator++() {
+			this->index_ptr_ += Size;
+
+			return *this;
+		}
+
+		inline BufferIterator<Size>& operator--() {
+			this->index_ptr_ -= Size;
+
+			return *this;
+		}
+
+		inline BufferIterator<Size>& operator+=(off_t offset) {
+			this->index_ptr_ += offset * Size;
+
+			return *this;
+		}
+
+		inline BufferIterator<Size>& operator-=(off_t offset) {
+			this->index_ptr_ -= offset * Size;
+
+			return *this;
+		}
+
+		inline BufferIterator<Size> operator+(off_t offset) const {
+			BufferIterator<Size> tmpIter(*this);
+			tmpIter += offset;
+
+			return tmpIter;
+		}
+
+		inline BufferIterator<Size> operator-(off_t offset) const {
+			BufferIterator<Size> tmpIter(*this);
+			tmpIter -= offset;
+
+			return tmpIter;
+		}
+
+		inline BufferIterator<Size> operator[](off_t offset) const {
+			return *(this->operator+(offset));
+		}
+
+	private:
+		uint8_t* index_ptr_;
+
+};
+
+
+}
+
+#endif  // BINARY_MAPPING_SRC_IO_BUFFER_ITERATOR_H_
