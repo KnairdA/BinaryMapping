fff9a0e4615303c74465b274b044cffc979804bd
Adrian Kummerländer <KnairdA@t-online.de>
2014-01-21 19:48:13 +0100

    Added type check to UndefinedEndian * as both LittleEndian and BigEndian member template methods are limited to integral types this check should also be enforced for UndefinedEndian to match the supported types

--- src/endianess/undefined.h
+++ src/endianess/undefined.h
@@ -6,12 +6,18 @@
 namespace BinaryMapping {
 
 struct UndefinedEndian {
-	template <typename Key>
+	template <
+		typename Key,
+		enable_if<std::is_integral<Key>::value> = 0
+	>
 	static inline Key toTarget(ConstLValueReference<Key> number) {
 		return number;
 	}
 
-	template <typename Key>
+	template <
+		typename Key,
+		enable_if<std::is_integral<Key>::value> = 0
+	>
 	static inline Key toHost(ConstLValueReference<Key> number) {
 		return number;
 	}
