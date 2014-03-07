435aed2b5bec1d5fe06973f0c9f06bba6c797007
Adrian Kummerländer <KnairdA@t-online.de>
2014-03-07 22:18:22 +0100

    Added custom type implementation test case * the test case contained within the special use case tests implements a simple std::pair like type to map values ** it then instantiates a container specialized on this custom type and tests if custom types are basically supported * this test cases also shows that some of the templates in the dtl namespace can be helpful to custom type implementations ** i.e. dtl::BasePointer and dtl::RelativePointer ** a helper template for defining the base type alias template could also be helpful

--- tests/special_use_case.cc
+++ tests/special_use_case.cc
@@ -3,6 +3,9 @@
 #include "container.h"
 #include "type/tuple.h"
 
+#include "detail/base_pointer.h"
+#include "detail/relative_pointer.h"
+
 class SpecialUseCaseTest : public ::testing::Test { };
 
 TEST_F(SpecialUseCaseTest, ContainerNesting) {
@@ -51,5 +54,89 @@ TEST_F(SpecialUseCaseTest, ContainerNesting) {
 			EXPECT_EQ((*level2).get<0>(), level2 - nestedContainer.begin());
 			EXPECT_EQ((*level2).get<1>(), level2 - nestedContainer.begin());
 		}
+	}
+}
+
+template <
+	typename Base,
+	typename Type1,
+	typename Type2
+>
+class PairImpl {
+	public:
+		typedef std::pair<Type1, Type2> value_type;
+
+		static const size_t size = sizeof(Type1) + sizeof(Type2);
+
+		template <typename BaseType>
+		PairImpl(BaseType data):
+			base_ptr_(data),
+			first_(base_ptr_.get(), 0),
+			second_(base_ptr_.get(), sizeof(Type1)) { }
+
+		inline operator value_type() const {
+			return std::make_pair(
+				*this->first_.get(),
+				*this->second_.get()
+			);
+		}
+
+		inline Type1 first() const {
+			return *this->first_.get();
+		}
+
+		inline Type2 second() const {
+			return *this->second_.get();
+		}
+
+		inline void operator=(const value_type& values) {
+			*this->first_.get()  = values.first;
+			*this->second_.get() = values.second;
+		}
+
+	private:
+		const BinaryMapping::dtl::BasePointer<Base> base_ptr_;
+
+		BinaryMapping::dtl::RelativePointer<Base, Type1> first_;
+		BinaryMapping::dtl::RelativePointer<Base, Type2> second_;
+
+};
+
+template <
+	typename Type1,
+	typename Type2
+>
+struct Pair {
+	template <typename Base>
+	using type = typename std::conditional<
+		std::is_const<Base>::value,
+		PairImpl<
+			Base,
+			typename std::add_const<Type1>::type,
+			typename std::add_const<Type2>::type
+		>,
+		PairImpl<Base, Type1, Type2>
+	>::type;
+};
+
+TEST_F(SpecialUseCaseTest, CustomType) {
+	BinaryMapping::Container<
+		Pair<uint32_t, uint8_t>
+	> container(10);
+
+	for ( auto iter = container.begin();
+	      iter     != container.end();
+	      ++iter ) {
+		(*iter) = std::pair<uint32_t, uint8_t>(
+			iter - container.begin() + 2,
+			iter - container.begin()
+		);
+	}
+
+	for ( auto iter = container.begin();
+	      iter     != container.end();
+	      ++iter ) {
+		EXPECT_EQ((*iter).first(),  iter - container.begin() + 2);
+		EXPECT_EQ((*iter).second(), iter - container.begin());
 	}
 }
