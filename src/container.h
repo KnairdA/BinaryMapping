2e02e2a282918e036d1c9d51bb2b552dd24d2af8
Adrian Kummerländer <KnairdA@t-online.de>
2014-03-11 17:07:46 +0100

    Added size_type to Container template and use dtl::Buffer typedefs * if dtl::Buffer offers buffer pointer and size types, why not use them? * the goal is to have all types where it makes sense centrally defined

--- src/container.h
+++ src/container.h
@@ -17,6 +17,7 @@ namespace BinaryMapping {
 template <typename Type>
 class Container {
 	public:
+		typedef std::size_t size_type;
 		typedef typename Type::template type<std::uint8_t> element_type;
 		typedef typename Type::template type<const std::uint8_t> const_element_type;
 
@@ -30,30 +31,30 @@ class Container {
 			const_element_type
 		> const_iterator_type;
 
-		explicit Container(std::size_t size):
+		explicit Container(size_type size):
 			buffer_(size * element_type::size),
 			count_(buffer_.size<element_type::size>()) { }
 
-		template <std::size_t Size>
+		template <size_type Size>
 		explicit Container(ByteField<Size>* field):
 			buffer_(field->bytes.data(), field->bytes.size()),
 			count_(buffer_.size<element_type::size>()) { }
 
-		Container(std::uint8_t*const ptr, std::size_t size):
+		Container(dtl::Buffer::pointer ptr, dtl::Buffer::size_type size):
 			buffer_(ptr, size),
 			count_(buffer_.size<element_type::size>()) { }
 
-		inline std::size_t size() const {
+		inline size_type size() const {
 			return this->count_;
 		}
 
-		inline const_element_type operator[](std::size_t index) const {
+		inline const_element_type operator[](size_type index) const {
 			return const_element_type(
 				this->buffer_[element_type::size * index]
 			);
 		}
 
-		inline const_element_type at(std::size_t index) const {
+		inline const_element_type at(size_type index) const {
 			return const_element_type(
 				this->buffer_.template at<element_type::size>(index)
 			);
@@ -84,19 +85,22 @@ class Container {
 		}
 
 		inline std::pair<
-			const std::uint8_t*const,
-			const std::size_t
+			const dtl::Buffer::pointer,
+			const dtl::Buffer::size_type
 		> data() const {
-			return std::make_pair(this->buffer_.front(), this->buffer_.size());
+			return std::make_pair(
+				this->buffer_.front(),
+				this->buffer_.size()
+			);
 		}
 
-		inline element_type operator[](std::size_t index) {
+		inline element_type operator[](size_type index) {
 			return element_type(
 				this->buffer_[element_type::size * index]
 			);
 		}
 
-		inline element_type at(std::size_t index) {
+		inline element_type at(size_type index) {
 			return element_type(
 				this->buffer_.template at<element_type::size>(index)
 			);
@@ -127,15 +131,18 @@ class Container {
 		}
 
 		inline std::pair<
-			std::uint8_t*const,
-			const std::size_t
+			const dtl::Buffer::pointer,
+			const dtl::Buffer::size_type
 		> data() {
-			return std::make_pair(this->buffer_.front(), this->buffer_.size());
+			return std::make_pair(
+				this->buffer_.front(),
+				this->buffer_.size()
+			);
 		}
 
 	private:
 		dtl::Buffer  buffer_;
-		const std::size_t count_;
+		const size_type count_;
 
 };
 
